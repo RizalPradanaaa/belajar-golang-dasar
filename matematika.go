37ddad6e45877b17c16996c9428c757a916c4f58
RizalPradanaaa <rizalpradanaaaa@gmail.com>
2023-11-22 09:49:34 +0700

    Operator Matematika

---
+++ matematika.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Operator matematika
+	angka1 := 10
+	angka2 := 20
+
+	total := angka1 + angka2
+	fmt.Println(total)	// 30
+
+
+	// Augmented Assignment
+	angka := 10
+	angka += 10
+	fmt.Println(angka)	//20
+
+
+	// Unary Operator
+	i := 1
+	i++
+	i++
+	fmt.Println(i) // 3
+}
